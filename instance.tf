ec3c8117060d0f7a42e86d29f664be18238d7676
Achal Sunil Padol <achalpadol@gmail.com>
2026-02-27 19:19:22 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,10 +1,126 @@
 provider "aws" {
-    region = "ap-south-1"
-  
+  region = "ap-south-1"
 }
-resource "aws_instance" "fct" {
-    ami = "ami-051a31ab2f4d498f5"
-    instance_type = "t3.micro"
-    key_name = "achal-mumbai-key"
-  
+
+# ---------------- VPC ----------------
+resource "aws_vpc" "myvpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "My-VPC"
+  }
+}
+
+# ---------------- Subnet ----------------
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public-Subnet"
+  }
+}
+
+# ---------------- Internet Gateway ----------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+
+  tags = {
+    Name = "My-IGW"
+  }
+}
+
+# ---------------- Route Table ----------------
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "Public-Route-Table"
+  }
+}
+
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "rt_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# ---------------- Security Group ----------------
+resource "aws_security_group" "mysg" {
+  name   = "my-security-group"
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # SSH
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # HTTP
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # MySQL (for demo only)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ---------------- EC2 Instances ----------------
+resource "aws_instance" "jump_server" {
+  ami                         = "ami-0f5ee92e2d63afc18"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.mysg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Jump-Server"
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami                         = "ami-0f5ee92e2d63afc18"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.mysg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Application-Server"
+  }
+}
+
+resource "aws_instance" "db_server" {
+  ami                         = "ami-0f5ee92e2d63afc18"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.mysg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "DB-Server"
+  }
 }
