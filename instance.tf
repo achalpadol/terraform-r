41df3f166876b213b1c0d8351e35d4e944a11862
Achal Sunil Padol <achalpadol@gmail.com>
2026-02-27 17:22:14 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -5,7 +5,6 @@ provider "aws" {
 resource "aws_instance" "fct" {
     ami = "ami-051a31ab2f4d498f5"
     instance_type = "t3.micro"
-    security_groups = "sg-023210fd407bf165a"
     key_name = "achal-mumbai-key"
   
 }
