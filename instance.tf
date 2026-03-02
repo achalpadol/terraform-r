463b2badda406047f5d2fb0c35e19cb3cb1d9093
Achal Sunil Padol <achalpadol@gmail.com>
2026-03-02 17:11:12 +0530

    create load balancer

--- instance.tf
+++ instance.tf
@@ -2,20 +2,6 @@ provider "aws" {
     region = "ap-south-1"
   
 }
-resource "aws_instance" "first" {
-ami = "ami-051a31ab2f4d498f5"  
-instance_type = "t3.micro"
-key_name = "achal-mumbai-key"
-vpc_security_group_ids = [aws_security_group.my-sg.id]
-tags = {
-  Name = "ec2-instance"
-}
-
-resource "aws_key_pair" "generated_key" {
-  key_name   = "generated-key"
-  public_key = tls_private_key.example_ssh.public_key_openssh
-}
-  
 
 resource "aws_security_group" "my-sg" {
     name = "fct-sg"
@@ -41,4 +27,54 @@ resource "aws_security_group" "my-sg" {
         cidr_blocks = [ "0.0.0.0/0" ]
     }
   
+}
+resource "aws_launch_template" "web_template" {
+  name_prefix   = "web-template"
+  image_id      = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              yum install -y httpd
+              systemctl start httpd
+              echo "Hello from Terraform ASG" > /var/www/html/index.html
+              EOF
+  )
+}
+resource "aws_lb" "app_lb" {
+  name               = "app-load-balancer"
+  load_balancer_type = "application"
+  subnets            = ["subnet-0fa61c74694df6bf4", "subnet-023dc2dad10b7c274"] 
+  security_groups    = [aws_security_group.web_sg.id]
+}
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "vpc-008594ecdb58c132a"  # Replace with your VPC ID
+}
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+resource "aws_autoscaling_group" "web_asg" {
+  desired_capacity     = 2
+  max_size             = 3
+  min_size             = 1
+  vpc_zone_identifier  = ["subnet-0fa61c74694df6bf4", "subnet-023dc2dad10b7c274"]  # Same subnets
+
+  launch_template {
+    id      = aws_launch_template.web_template.id
+    version = "$Latest"
+  }
+
+  target_group_arns = [aws_lb_target_group.app_tg.arn]
 }
