a473c527b625b3d5446159af2cbc4c645f0d5360
Achal Sunil Padol <achalpadol@gmail.com>
2026-03-02 16:03:59 +0530

    add key

--- instance.tf
+++ instance.tf
@@ -27,6 +27,13 @@ resource "aws_security_group" "my-sg" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    ingress {
+        description = "allow port"
+        to_port = 80
+        from_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     egress {
         description = "allow port"
         to_port = 0
