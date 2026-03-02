a2a1407382a6638145e4a36b2cb254fa166201ed
Achal Sunil Padol <achalpadol@gmail.com>
2026-03-02 15:19:43 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "first" {
 ami = "ami-051a31ab2f4d498f5"  
 instance_type = "t3.micro"
 key_name = "achal-mumbai-key"
-vpc_security_group_ids = [aws_security_group.my-sg.vpc_id]
+vpc_security_group_ids = [aws_security_group.my-sg.id]
 tags = {
   Name = "ec2-instance"
 }
